a6182468dbcb4ad44d58060777e3f1b4a52d39ef
Ouadra <mohamed.oudra3@gmail.com>
2024-11-28 02:03:56 +0100

    Authentification Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Controllers\Auth;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('auth/register', 'Auth::registerView');  // To display the registration form
+$routes->post('auth/register', 'Auth::register');      // To handle the form submission and store data in the database
+$routes->get('auth/login', 'Auth::loginView');
+$routes->post('auth/login', 'Auth::login');
