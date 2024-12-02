2c5b6797629bb415a5dbd6f32e4179c8ddda77c9
Ali HALLA <ahalla884@gmail.com>
2024-12-02 18:35:07 +0100

    Update Bus_Route_view

--- app/Views/Bus_Route_view.php
+++ app/Views/Bus_Route_view.php
@@ -288,37 +288,3 @@
 	</script>
 	
 <?= $this->endSection() ?>
-<td>
-									<div class="dropdown">
-										<a
-											class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle"
-											href="#"
-											role="button"
-											data-toggle="dropdown"
-										>
-											<i class="dw dw-more"></i>
-										</a>
-										<div
-											class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-										>
-										<button class="dropdown-item"
-												data-id="<?= esc($bus_route['id_bus_route']) ?>" 
-												data-id-bus="<?= esc($bus_route['id_bus']) ?>" 
-												data-id-route="<?= esc($bus_route['id_route']) ?>" 
-												data-depart="<?= esc($bus_route['heure_depart']) ?>" 
-												data-arrivee="<?= esc($bus_route['heure_arrivee']) ?>" 
-												data-cost="<?= esc($bus_route['prix']) ?>" 
-												data-toggle="modal" 
-												data-target="#editBusRouteModal">
-											<i class="dw dw-edit2"></i> Edit
-										</button>
-										<button class="dropdown-item" 
-											data-id="<?= esc($bus_route['id_bus_route']) ?>" 
-											data-toggle="modal" 
-											data-target="#deleteBusRouteModal">
-											<i class="dw dw-delete-3"></i>Delete
-										</button>
-											
-										</div>
-									</div>
-								</td>
