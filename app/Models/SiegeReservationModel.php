3f3dbfd49bd0a16eb8405e401ee66004f86be8e0
Ali HALLA <ahalla884@gmail.com>
2024-12-02 18:35:07 +0100

    Update SiegeReservation Model

--- app/Models/SiegeReservationModel.php
+++ app/Models/SiegeReservationModel.php
@@ -20,14 +20,12 @@ class SiegeReservationModel extends Model
     protected array $casts = [];
     protected array $castHandlers = [];
 
-    // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
-    // Validation
     protected $validationRules      = [
         'id_reservation' => 'required|integer',
         'id_siege'       => 'required|integer',
@@ -37,7 +35,6 @@ class SiegeReservationModel extends Model
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
-    // Callbacks
     protected $allowCallbacks = true;
     protected $beforeInsert   = [];
     protected $afterInsert    = [];
@@ -47,4 +44,15 @@ class SiegeReservationModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+    
+    public function getSiegesreservesWithDetails()
+    {
+    return $this->select('siege_reservations.*, clients.id_client, routes.id_route, buses.id_bus, buses.nom_bus, routes.ville_depart, routes.ville_arrivee, sieges.numero_siege')
+                ->join('reservations', 'reservations.id_reservation = siege_reservations.id_reservation')
+                ->join('clients', 'clients.id_client = reservations.id_client')
+                ->join('sieges', 'sieges.id_siege = siege_reservations.id_siege')
+                ->join('buses', 'buses.id_bus = sieges.id_bus')
+                ->join('routes', 'routes.id_route = reservations.id_route')
+                ->findAll();
+    }
 }
