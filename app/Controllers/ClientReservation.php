990104fbf471b6ede0adae137998174ef87006be
Ali HALLA <ahalla884@gmail.com>
2024-12-12 02:04:03 +0100

    Update Client_Controllers§§

--- app/Controllers/ClientReservation.php
+++ app/Controllers/ClientReservation.php
@@ -28,26 +28,12 @@ class ClientReservation extends BaseController
 
         ];
 
-        // Passer les données à la vue
         return view('client_reservation_view', $data);
-        
-        // $SiegeReservationModel = new SiegeReservationModel();
-        // $busRouteModel = new BusRouteModel();
-        
-        // $busRoutes = [];
-    
-        // $data = [
-        //     'SiegeReservations' => $SiegeReservationModel->getSiegesreservesWithDetails(),
-        //     'BusRoutes' => $busRoutes,
-        // ];
-    
-        // return view('client_reservation_view', $data);
     }
 
     public function search()
 {
 
-    // Charger le modèle pour les routes
     $routeModel = new RouteModel();
     $routes = $routeModel->findAll();
 
@@ -60,9 +46,8 @@ class ClientReservation extends BaseController
 
         $busRouteModel = new BusRouteModel();
         $busRoutes = $busRouteModel
-            ->where('bus_routes.id_route', $routeId)  // Utilisation de l'alias 'br'
-            // ->where('siege_reservations.date_depart', $dateDepart)
-            ->getSiegeWithDetails();
+            ->where('bus_routes.id_route', $routeId)
+            ->getBusRoutesWithDetails();
         
         $availableSeats = $SiegeReservationModel->getSiegesreservesWithDetails();
         
@@ -77,9 +62,9 @@ class ClientReservation extends BaseController
         // Ajoutez le prix à l'HTML
         }
 
-        if (empty($busRoutes) || empty($availableSeats)) {
+        if (!$busRoutes) {
             // Aucun bus disponible ou pas de réservations pour cette date
-            return redirect()->back()->with('error', 'Aucun bus trouvé pour cette route et cette date.');
+            return redirect()->back()->with('error', 'No buses found for this route');
         }
 
         // Passer les données à la vue
@@ -134,6 +119,8 @@ public function showSeats()
             'Sieges' => $Siege,
             'id_route' => $id_route,
             'dateDepart' => $date_depart,
+            'id_bus' => $id_bus,
+
         ];
 
         return view('client_reservation_view', $data);
@@ -146,23 +133,18 @@ public function showSeats()
 public function reserveSeat()
     {
         if ($this->request->getMethod() === 'POST') {
-            // Récupérer les données envoyées par le formulaire
+
             $selected_seat = $this->request->getPost('selected_seat'); 
-            $id_client = $this->request->getPost('id_client'); 
-            $id_siege = $this->request->getPost('id_siege');
+            // $id_client = $this->request->getPost('id_client'); 
             $id_route = $this->request->getPost('id_route');  
             $id_bus = $this->request->getPost('id_bus');  
             $date_depart = $this->request->getPost('date_depart'); 
-            $idd_siege_reservation = $this->request->getPost('id_siege_reservation'); 
-            $id_reservation = $this->request->getPost('id_reservation'); 
-            $id_reservation_current = $this->request->getPost('id_reservation_current'); 
-
 
             $SiegeReservationModel = new SiegeReservationModel();
             $ReservationModel = new ReservationModel();
             $SiegeModel = new SiegeModel();
 
-            if (!$selected_seat || !$id_client || !$id_siege || !$id_route || !$id_bus || !$date_depart) {
+            if (!$selected_seat  || !$id_route || !$id_bus || !$date_depart) {
                 return $this->response->setStatusCode(400)->setJSON([
                     'status' => 'error',
                     'message' => 'Veuillez remplir tous les champs requis.'
@@ -177,31 +159,17 @@ public function reserveSeat()
             if (!$Siege) {
                 return $this->response->setStatusCode(404)->setJSON([
                     'status' => 'error',
-                    'message' => 'Le siège sélectionné est introuvable.'
-                ]);
-            }   
-    
-            $siegereserve = $SiegeReservationModel
-                ->where('siege_reservations.id_siege', $id_siege)
-                ->first();
-    
-            if (!$siegereserve) {
-                return $this->response->setStatusCode(404)->setJSON([
-                    'status' => 'error',
-                    'message' => 'La réservation correspondante est introuvable.'
+                    'message' => 'The selected seat was not found'
                 ]);
             }
             
-            $reservation = $ReservationModel
-                ->where('reservations.id_siege', $id_siege)
-                ->first();
-    
+            $id_client = session()->get('id');
 
             // Créer la réservation
             $reservationData = [
                 'id_siege' => $Siege['id_siege'],
-                'id_client' => 3,
-                'id_route' => $reservation['id_route'],
+                'id_client' => $id_client,
+                'id_route' => $id_route,
                 'ticket_code' => strtoupper(uniqid('TICKET_')), // Génération d'un code de ticket unique
                 'date_reservation' => date('Y-m-d H:i:s'),
                 'created_at'    => date('Y-m-d H:i:s'),
@@ -215,7 +183,7 @@ public function reserveSeat()
 
                 // Ajouter la réservation dans la table siege_reservations
                 $siegeReservationData = [
-                    'id_reservation' => $reservation['id_reservation'],
+                    'id_reservation' => $reservationId,
                     'id_siege' => $Siege['id_siege'],
                     'date_depart' => $date_depart,
                     'created_at'    => date('Y-m-d H:i:s'),
@@ -224,47 +192,22 @@ public function reserveSeat()
 
                 // Insertion dans siege_reservations
                 if ($SiegeReservationModel->insert($siegeReservationData)) {
-                    session()->setFlashdata('success', 'Bus updated successfully!');
+                    session()->setFlashdata('success', 'Reservation added successfully!');
                 }else {
-                    session()->setFlashdata('error', 'Failed to update Bus.');
+                    session()->setFlashdata('error', 'Failed to added Reservation.');
                 }
                 
             }else {
-                session()->setFlashdata('error', 'Failed to update Bus.');
+                session()->setFlashdata('error', 'Failed to added Reservation.');
             }
 
-            return redirect()->to('/reservations/3');
+            return redirect()->to("/reservations/$id_client");
         }
 
         return $this->response->setStatusCode(405)->setBody('Method Not Allowed');
     }
 
 
-
-
-
-
-
-    // public function getAvailableSeats()
-    // {
-    //     $busId = $this->request->getVar('bus_id');
-    //     $routeId = $this->request->getVar('route_id');
-    //     $date = $this->request->getVar('date');
-
-    //     $siegeModel = new SiegeModel();
-
-    //     $seats = $siegeModel
-    //         ->where('id_bus', $busId)
-    //         ->where('id_route', $routeId)
-    //         ->where('date_reservation !=', $date) // Siège non réservé
-    //         ->findAll();
-
-    //     if ($seats) {
-    //         return $this->response->setJSON(['success' => true, 'seats' => $seats]);
-    //     } else {
-    //         return $this->response->setJSON(['success' => false, 'message' => 'Aucun siège disponible.']);
-    //     }
-    // }
 
 }
 
