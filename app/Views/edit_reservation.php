710526731321dca427f133d236a66953f17953bc
Ali HALLA <ahalla884@gmail.com>
2024-12-12 17:30:20 +0100

    Update edit_reservation§

--- app/Views/edit_reservation.php
+++ app/Views/edit_reservation.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recherche des Bus</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+<?= $this->extend('Views\side_bare_client') ?>
+
+<?= $this->section('content') ?>
     <style>
         body {
             display: flex;
@@ -178,6 +174,5 @@
         <?php endif; ?>
 
     </div>
-</body>
-</html>
+    <?= $this->endSection() ?>
 
